534baccad377274530ed3884fd0c237b78a41703
Drew Walton <drewwalton19216801@gmail.com>
2015-02-05 15:28:51 -0700

    Add iniparser
    We use iniparser for themes and settings
    
    Change-Id: I72287bb6a3fae60086eb9568f5f7358220cf2244

--- cot/Android.mk
+++ cot/Android.mk
@@ -16,32 +16,42 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := libcot
 LOCAL_MODULE_TAGS := eng
+
 define all-cpp-files-under
 $(patsubst ./%,%, \
-$(shell cd $(LOCAL_PATH) ; \
-find $(1) -name "*.cpp" -and -not -name ".*") \
+    $(shell cd $(LOCAL_PATH) ; \
+        find $(1) -name "*.cpp" -and -not -name ".*") \
 )
 endef
+
 LOCAL_SRC_FILES += $(call all-cpp-files-under, .)
 
+LOCAL_SRC_FILES += \
+    iniparser/iniparser.c \
+    iniparser/dictionary.c
+
 LOCAL_CFLAGS += -DUSE_EXT4 -DMINIVOLD
+
 LOCAL_C_INCLUDES += \
-system/extras/ext4_utils \
-system/core/fs_mgr/include \
-external/fsck_msdos \
-system/vold \
-system/code/healthd \
-system/core/include \
-system/core/libcutils \
-external/libtar \
-external/libtar/listhash \
-external/zlib \
-bionic/libc/bionic
+    system/extras/ext4_utils \
+    system/core/fs_mgr/include \
+    external/fsck_msdos \
+    system/vold \
+    system/code/healthd \
+    system/core/include \
+    system/core/libcutils \
+    external/libtar \
+    external/libtar/listhash \
+    external/zlib \
+    bionic/libc/bionic
+
 ifeq ($(TARGET_USERIMAGES_USE_F2FS), true)
-LOCAL_CFLAGS += -DUSE_F2FS
-LOCAL_STATIC_LIBRARIES += libmake_f2fs libfsck_f2fs libfibmap_f2fs
+    LOCAL_CFLAGS += -DUSE_F2FS
+    LOCAL_STATIC_LIBRARIES += libmake_f2fs libfsck_f2fs libfibmap_f2fs
 endif
+
 ifneq ($(BOARD_RECOVERY_BLDRMSG_OFFSET),)
-LOCAL_CFLAGS += -DBOARD_RECOVERY_BLDRMSG_OFFSET=$(BOARD_RECOVERY_BLDRMSG_OFFSET)
+    LOCAL_CFLAGS += -DBOARD_RECOVERY_BLDRMSG_OFFSET=$(BOARD_RECOVERY_BLDRMSG_OFFSET)
 endif
+
 include $(BUILD_STATIC_LIBRARY)
